3e86e85ee3355dffe859a1c0f88989c818def73f
Ao Song <ao.song@outlook.com>
2019-12-08 21:33:36 +0100

    fix indentation

--- kv/kv.go
+++ kv/kv.go
@@ -1,27 +1,27 @@
 package kv
 
 import (
-	"sync"
+    "sync"
 )
 
 type Store struct {
-	KV  map[string]interface{}
-	Mux sync.RWMutex
+    KV  map[string]interface{}
+    Mux sync.RWMutex
 }
 
 func (s *Store) Set(k string, v interface{}) {
-	s.Mux.Lock()
-	s.KV[k] = v
-	s.Mux.Unlock()
+    s.Mux.Lock()
+    s.KV[k] = v
+    s.Mux.Unlock()
 }
 
 func (s *Store) Get(k string) (interface{}, bool) {
-	v, found := s.KV[k]
-	return v, found
+    v, found := s.KV[k]
+    return v, found
 }
 
 func (s *Store) Del(k string) {
-	s.Mux.Lock()
-	delete(s.KV, k)
-	s.Mux.Unlock()
+    s.Mux.Lock()
+    delete(s.KV, k)
+    s.Mux.Unlock()
 }

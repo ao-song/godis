dbfeae1faf3eea4dbdeacf60ec119978f4dd6a2e
Ao Song <ao.song@outlook.com>
2019-12-16 19:38:10 +0100

    format

--- kv/godis.go
+++ kv/godis.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-	"net"
-	"log"
-	"encoding/json"
-	"godis/kv"
+    "net"
+    "log"
+    "encoding/json"
+    "godis/kv"
 )
 
 type request struct {
-	action string
-	value  string
+    action string
+    value  string
 }
 
 func main() {
-	s := &kv.Store{KV: make(map[string]interface{})}
+    s := &kv.Store{KV: make(map[string]interface{})}
 
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
+    ln, err := net.Listen("tcp", ":8080")
+    if err != nil {
         log.Fatal("TCP server listen error:", err)
-	}
+    }
 
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatal("TCP server accept error:", err)
-		}
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            log.Fatal("TCP server accept error:", err)
+        }
 
-		go handleConnection(conn, s)
-	}
+        go handleConnection(conn, s)
+    }
 }
 
 func handleConnection(conn net.Conn, s *kv.Store) {
-	decoder := json.NewDecoder(conn)
+    decoder := json.NewDecoder(conn)
 
-	for {
-		var req request
+    for {
+        var req request
 
-		err := decoder.Decode(&req)
-		if err != nil {
-			log.Fatal("JSON decode error:", err)
-		}
+        err := decoder.Decode(&req)
+        if err != nil {
+            log.Fatal("JSON decode error:", err)
+        }
 
-		
-	}
-	
-	
+        
+    }
+    
+    
 }
 
